a3b184faa90238d259fc2e62bd63e6d26ce0bb0c
Jordano <Jordano470@gmail.com>
2022-07-06 17:26:05 -0500

    finished Css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import setTheme from './theme';
 import { setLightTheme } from './theme';
 import crossOutCheckbox from './EditTask'
+import {initializePopUp} from './ShowPopUp'
 
 setLightTheme()
 crossOutCheckbox()
+initializePopUp()
