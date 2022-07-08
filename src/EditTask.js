8e8a7b45f10430d7790fef54907622fe064e0099
Jordano <Jordano470@gmail.com>
2022-07-07 19:31:34 -0500

    Added more task functions

--- src/EditTask.js
+++ src/EditTask.js
@@ -1,24 +1,18 @@
-function crossOutCheckbox(){
-    const checkTask= document.querySelector('.checkmark');
-    checkTask.addEventListener('click', crossOutTask)
- 
-}
 
-function crossOutTask(){ 
-    const taskText = document.querySelector('.aboutTask')
-    const date = document.querySelector('.date')
-    let checkTask= document.querySelector('#task').checked;
-    if(checkTask === false){
-        taskText.setAttribute('style', 'text-decoration: line-through; color: var(--textOn-color);')
-        date.setAttribute('style', 'color: var(--textOn-color);')
+function crossOutTask(e){ 
+    if( e.currentTarget.checked === false){
+        e.currentTarget.parentElement.parentElement.removeAttribute('style', ' text-decoration: line-through; color: var(--textOn-color);')
+        console.log('2')
+        
+    }
+    else if( e.currentTarget.checked === true){
+        e.currentTarget.parentElement.parentElement.setAttribute('style', ' text-decoration: line-through; color: var(--textOn-color);')
         console.log('1')
     }
-    else if(checkTask === true){
-        taskText.removeAttribute('style', 'text-decoration: line-through; color: var(--textOn-color);')
-        date.removeAttribute('style', 'color: var(--textOn-color);')
-        console.log('2')
-    }
+}
+function deleteTask(e){ 
+    e.currentTarget.parentElement.parentElement.parentElement.remove()
 }
     
 
-export default crossOutCheckbox
+export {crossOutTask, deleteTask}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 import setTheme from './theme';
 import { setLightTheme } from './theme';
-import crossOutCheckbox from './EditTask'
+
 import {initializePopUp} from './ShowPopUp'
 
 setLightTheme()
-crossOutCheckbox()
+
 initializePopUp()
