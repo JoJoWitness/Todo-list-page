23e5ae40a18dfc3a5050f7cb10432f3d93361401
Jordano <Jordano470@gmail.com>
2022-07-13 18:57:07 -0500

    ?

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        options: {
+          publicPath: ''
+        }
       },
       {
         test: /\.m?js$/,
